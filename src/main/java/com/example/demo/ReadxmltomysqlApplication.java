18786c06c9e5b7ba189d1995c9cd8cd911a1630c
AnushaShiva <kollianusha23@gmail.com>
2023-12-18 14:13:24 +0530

    added sop in main class

--- src/main/java/com/example/demo/ReadxmltomysqlApplication.java
+++ src/main/java/com/example/demo/ReadxmltomysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReadxmltomysqlApplication {
 
 	public static void main(String[] args) {
+         System.out.println();
 		SpringApplication.run(ReadxmltomysqlApplication.class, args);
 	}
 
