863998672864b03aebef982759fd52322bfded4b
AnushaShiva <97225826+AnushaShiva@users.noreply.github.com>
2023-12-19 12:21:18 +0530

    Update ReadxmltomysqlApplication.java

--- src/main/java/com/example/demo/ReadxmltomysqlApplication.java
+++ src/main/java/com/example/demo/ReadxmltomysqlApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReadxmltomysqlApplication {
 
 	public static void main(String[] args) {
-         System.out.println();
+         System.out.println("debuggig perpouse");
 		SpringApplication.run(ReadxmltomysqlApplication.class, args);
 	}
 
